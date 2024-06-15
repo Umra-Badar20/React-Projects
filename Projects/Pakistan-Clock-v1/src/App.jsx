616e2a408cabdea9557cc0d400acca1c63b95e52
Umra-Badar20 <umrabadar555@gmail.com>
2024-06-15 17:47:22 +0500

    Styling done

---
+++ Projects/Pakistan-Clock-v1/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import ClockHeading from './Components/ClockHeading'
+import ClockSlogan from './Components/ClockSlogan'
+import CurrentTime from './Components/Current time'
+
+function App() {
+ return <center>
+    <ClockHeading></ClockHeading>
+    <ClockSlogan></ClockSlogan>
+    <CurrentTime></CurrentTime>
+ </center>
+}
+export default App
---
+++ Projects/Pakistan-Clock-v1/src/Components/ClockHeading.jsx
@@ -0,0 +1,4 @@
+function ClockHeading(){
+return <h1 className="fw-bold">Pakistan Clock</h1>
+}
+export default ClockHeading;
